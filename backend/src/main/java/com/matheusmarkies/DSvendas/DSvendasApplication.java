57aba9dcf35d1179088b46725da1a5ed30dfbfee
Matheus Markies <matheus.markies@gmail.com>
2021-11-01 21:01:17 -0300

    Project created

---
+++ backend/src/main/java/com/matheusmarkies/DSvendas/DSvendasApplication.java
@@ -0,0 +1,13 @@
+package com.matheusmarkies.DSvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DSvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DSvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/matheusmarkies/DSvendas/DSvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusmarkies.DSvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DSvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
